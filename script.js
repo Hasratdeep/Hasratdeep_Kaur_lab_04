cd4098fec028ddf25d988e0fe4a52dbb5fe7429d
Hasratdeep <hasratdeepkaur383@gmail.com>
2025-02-25 18:09:55 -0600

    Implemented the form submission and event listner for event REgistration form.

---
+++ script.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded",function(){
+    const form = document.getElementById("eventRegistrationForm");
+
+//Form submission Event Listner
+form.addEventListener("submit",function(event) {
+    event.preventDefault();
+    console.log("Form submission.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+})

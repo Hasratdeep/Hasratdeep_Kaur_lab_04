3a31e28417f7d0030b919bf978925caa2ef5576c
Hasratdeep <hasratdeepkaur383@gmail.com>
2025-02-26 14:33:01 -0600

    Validated the number input in script.js file

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ form.addEventListener("submit",function(event) {
     console.log("Form submission.");
     });
 });
+
+//Validate the number Input from numbers 1 to 15
+const numTicketsInput = document.getElementById("numTickets");
+const isValid = ValidateNumberInput(numTicketsInput, 1 , 5 , 15);
+if (isValid) {
+    console.log("Valid input of the ticket number.");
+} 
+else {
+    console.log("Invalid input of the ticket number.");
+}
